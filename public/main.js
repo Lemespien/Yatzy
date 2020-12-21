90a656536c3e489a478eb9c189af20ad4c9883ca
Lemespien <nilhogg@gmail.com>
2020-12-22 00:27:34 +0100

    hello world

---
+++ public/main.js
@@ -0,0 +1,323 @@
+var db = firebase.app().firestore();
+
+
+var GameData = {};
+var gameID = null;
+let unsubscribe;
+LoadLatest();
+async function LoadLatest() {
+    var historyRef = await db.collection('yatzy').orderBy('date_created', 'desc').limit(1);
+    gameID = await historyRef.get().then((collection) => {
+        if (collection.docs.length > 0) {
+            if (collection.docs[0].exists) {
+                GameData = collection.docs[0].data();
+                CreateBoard();
+                return collection.docs[0].id;
+            } 
+        } else 
+            console.error("no such document");
+    }).catch(error => console.error(error));
+    if (gameID == null) {
+        NewGame();
+    } else {
+        document.querySelector("#roomID").value = gameID;
+        unsubscribe = db.collection("yatzy").doc(gameID).onSnapshot((doc) => {
+            GameData = doc.data();
+            Object.values(players).forEach(player => {
+                player.playerData = GameData[player.playerData.id];
+            });
+            UpdateBoard();
+        });
+
+    }
+}
+
+async function GetSaveData(ID) {
+    let gameDoc = await db.collection("yatzy").doc(ID).get();
+    if (gameDoc) {
+        CleanUp();
+        GameData = gameDoc.data();
+        CreateBoard();
+        gameID = ID;
+        db.collection("yatzy").doc(ID).onSnapshot(doc => {
+            GameData = doc.data();
+            Object.values(players).forEach(player => {
+                player.playerData = GameData[player.playerData.id];
+            });
+            UpdateBoard();
+            document.querySelector("#roomID").value = ID;
+        })
+    } else NewGame();
+} 
+
+
+
+const body = document.querySelector("body");
+
+const PlayerData = {
+    "name": "Default",
+    "id": "defaultID",
+    "playerOrder": 0,
+    "ones": 0,
+    "twos": 0,
+    "threes": 0,
+    "fours": 0,
+    "fives": 0,
+    "sixes": 0,
+    "sum": 0,
+    "bonus": 0,
+    "pair": 0,
+    "twopair": 0,
+    "threepair": 0,
+    "toak": 0,
+    "foak": 0,
+    "fioak": 0,
+    "sstraight": 0,
+    "lstraight": 0,
+    "fstraight": 0,
+    "cabin": 0,
+    "house": 0,
+    "tower": 0,
+    "chance": 0,
+    "maxiyatzy": 0,
+    "total": 0,
+}
+
+function PlayerObject(playerName, playerNameFormatted, playerOrder) {
+    if(GameData[playerNameFormatted] != null) {
+        this.playerData = GameData[playerNameFormatted];
+    } else {
+        this.playerData = {...PlayerData}
+        this.playerData.name = playerName;
+        this.playerData.id = playerNameFormatted;
+        this.playerOrder = playerOrder;
+    }
+
+    this.UpdateSum = () => {
+        this.playerData.sum = this.playerData.ones + this.playerData.twos + this.playerData.threes +this.playerData.fours + this.playerData.fives + this.playerData.sixes;
+        if (this.playerData.sum >= 84) {
+            this.playerData.bonus = 100;
+            document.querySelector("#"+this.playerData.id +"-bonus").value = this.playerData.bonus;
+        }
+        console.log(this.playerData);
+        document.querySelector("#"+this.playerData.id +"-sum").value = this.playerData.sum;
+    }, 
+
+    this.AddNumber = (value, numberID) => {
+        let splitID = numberID.split("-");
+        if (isNaN(value) || value < 0) {
+            value = 0;
+            document.querySelector("#"+numberID).value = 0;
+            document.querySelector("#"+numberID).classList.add("failed");
+        } 
+        this.playerData[splitID[1]] = value;
+    },
+
+    this.UpdateTotal = () => {
+        let totalSum = 0;
+        for (const property in this.playerData) {
+            if (IsFunction(property)) continue;
+            if (property == "total" || property == "sum") continue;
+            if (this.playerData[property] < 0) continue;
+            totalSum += this.playerData[property];
+        }
+        this.playerData.total = totalSum;
+        document.querySelector("#"+ this.playerData.id +"-total").value = this.playerData.total;
+    }
+}
+let funWords = [
+    "Apples",
+    "Oranges",
+    "Banananaa",
+    "Peaches",
+    "Poop",
+    "Bloom",
+    "CheezeDoodles",
+    "BearNoms",
+    "Smash",
+    "Tiger",
+    "Jaguar",
+    "Batman",
+    "Superman",
+    "SpiderMan",
+    "WonderWoman",
+    "Link",
+    "Zelda",
+    "Zoira",
+    "Applejack",
+    "RainbowDash",
+    "FlutterShy",
+    "Twilight"
+]
+
+function GetNewRoomID() {
+    let RandomInt = Math.floor(Math.random()* funWords.length);
+    let id = funWords[RandomInt] + Math.floor(Math.random()*1000);
+    document.querySelector("#roomID").value = id;
+    return id;
+}
+function NewGame() {
+    if (unsubscribe) unsubscribe();
+    gameID = GetNewRoomID();
+    CleanUp();
+    SaveGame();
+    db.collection("yatzy").doc(gameID).onSnapshot((doc) => {
+        if (doc?.data()){
+            GameData = doc.data();
+            Object.values(players).forEach(player => {
+                player.playerData = GameData[player.playerData.id];
+            });
+            UpdateBoard();
+        }
+    });
+}
+
+function CleanUp() {
+    const allTDs = document.querySelectorAll("td");
+    Array.from(allTDs).forEach(td => td.parentNode.removeChild(td));
+    GameData = {};
+    Object.values(players).forEach(player => {
+        player.playerData = {};
+    })
+    
+}
+
+function CreateBoard() {
+    const PlayerKeys = Object.keys(GameData).filter(key => key != "date_created").sort(playerData => playerData.playerOrder);
+    console.log(PlayerKeys);
+    Object.keys(GameData).forEach(key => {
+        if (key != "date_created"){
+            CreatePlayer(GameData[key].name);
+        }
+    });
+    Object.values(players).forEach(player => {
+        player.UpdateSum();
+    });
+}
+function UpdateBoard() {
+    Object.keys(GameData).forEach(key => {
+        if (key != "date_created") {
+            const id = GameData[key].id;
+            Object.keys(GameData[key]).forEach(data => {
+                if (!IsFunction(data)) {
+                    document.querySelector("#"+id + "-" + data).value = GameData[key][data];
+                }
+            });
+        }
+    });
+}
+function IsFunction(property) {
+    switch (property) {
+        case "UpdateSum":
+        case "AddNumber":
+        case "UpdateTotal":
+        case "id":
+        case "name":
+        case "playerOrder":
+            return true;
+        default:
+            return false;
+    }
+}
+
+let players = {
+}
+
+let PlayerOrder = 0;
+function CreatePlayer(playerName) {
+    const playerNameFormatted = playerName.replaceAll(" ", "").toLowerCase();
+    const namesRow = document.querySelector("tr.name");
+    const nameElement = document.createElement("td");
+    nameElement.innerText = playerName;
+    nameElement.id = playerNameFormatted;
+    namesRow.appendChild(nameElement);
+    let playerObject= new PlayerObject(playerName, playerNameFormatted, PlayerOrder);
+    PlayerOrder++;
+    for (const property in playerObject.playerData) {
+        AddColumn(property, playerObject);
+    }
+
+    players[playerNameFormatted] = playerObject;
+}
+
+function AddColumn(number, playerObject) {
+    if (IsFunction(number)) return;
+    const row = document.querySelector(".numbers."+number);
+
+    let tdElement = document.createElement("td");
+    let numberInputElement = document.createElement("input");
+    numberInputElement.type = "number";
+    numberInputElement.value = 0;
+    numberInputElement.id = playerObject.playerData.id + "-" + number;
+    if (playerObject.playerData[number] != 0) {
+        numberInputElement.value = playerObject.playerData[number];
+    }
+    switch (number) {
+        case "ones":
+        case "twos":
+        case "threes":  
+        case "fours":
+        case "fives":
+        case "sixes":
+            numberInputElement.addEventListener("change", () =>  {
+                playerObject.AddNumber(parseInt(numberInputElement.value), numberInputElement.id)
+                playerObject.UpdateSum();
+                SaveGame();
+            });
+
+            break;
+        case "sum":
+        case "bonus":
+        case "total":
+            numberInputElement.disabled = true;
+        default:
+            numberInputElement.addEventListener("change", () =>  {
+                playerObject.AddNumber(parseInt(numberInputElement.value), numberInputElement.id); 
+                SaveGame();
+            });
+                
+            break;
+    }
+
+    tdElement.appendChild(numberInputElement);
+    row.appendChild(tdElement);
+}
+
+function UpdateTotals() {
+    Object.keys(players).forEach(key => {
+        players[key].UpdateTotal();
+    });
+    let winner = null;
+    let winnerSum = 0;
+    Object.keys(players).forEach(key => {
+        winner = players[key].playerData.total > winnerSum
+        ? players[key].name 
+        : winner;
+        winnerSum = players[key].playerData.total;
+    });
+}
+
+function hidePlayerInput() {
+    document.querySelector("#playerInputContainer").style.display = "none";
+    document.querySelector("#playerInputShowButton").style.display = "block";
+    SaveGame();
+}
+function showPlayerInput() {    
+    document.querySelector("#playerInputContainer").style.display = "block";
+    document.querySelector("#playerInputShowButton").style.display = "none";
+}
+
+function SaveGame() {
+    console.log("Saving Game");
+    let playerDataCollection = {
+        date_created: GameData.date_created || Date.now(),
+    }
+    Object.keys(players).forEach(key => {
+        playerDataCollection[players[key].playerData.id] = players[key].playerData;
+    });
+    if (gameID == null) {
+        gameID = GetNewRoomID();
+    }
+    db.collection("yatzy").doc(gameID).set(playerDataCollection).then((docRef) => {
+    }).catch((error)=> console.error(error));
+}
